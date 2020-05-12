98e902500cd559736e2c4cc95a9874f1ce385e20
Aniket Yadav <mail.aniketyadav07@gmail.com>
2020-05-12 23:58:20 +0530

    MongoDB Connected

--- server.js
+++ server.js
@@ -2,19 +2,44 @@ const express = require('express');
 
 const dotenv = require('dotenv');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
+//const logger = require('./middleware/logger');
+const colors = require('colors');
+
+const morgan = require('morgan');
+
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//Mount logger middleware
+//app.use(logger);
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-	console.log(`App Server running in ${process.env.NODE_ENV} listening on port ${PORT}!`);
+const server = app.listen(PORT, () => {
+	console.log(`App Server running in ${process.env.NODE_ENV} listening on port ${PORT}!`.yellow.bold);
+});
+
+//Handle Unhandled Promise Rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	//Close server and exit process
+	server.close(() => process.exit(1));
 });
