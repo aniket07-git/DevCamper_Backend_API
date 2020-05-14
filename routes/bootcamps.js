147072ea9a1a6d4c45534d43498c3cc0125a0107
Aniket Yadav <mail.aniketyadav07@gmail.com>
2020-05-15 01:20:14 +0530

    Calculated the average costlogic for the course

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,7 +9,13 @@ const {
 	getBootcampsInRadius
 } = require('../controllers/bootcamps');
 
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -35,6 +36,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
