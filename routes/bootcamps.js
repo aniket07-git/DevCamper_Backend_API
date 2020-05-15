fd7a5ee7c5e746a71c146b31cb15d36e83ee73bf
Aniket Yadav <mail.aniketyadav07@gmail.com>
2020-05-15 13:27:14 +0530

    Added Advanced Middleware Functionality

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,6 +10,9 @@ const {
 	bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -22,7 +25,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
